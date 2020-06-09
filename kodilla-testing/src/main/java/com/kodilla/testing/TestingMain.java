965e566439c21834c672dd75da1f148cb69d72f9
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2020-06-09 22:30:30 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,12 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator = new Calculator(2,3);
+        System.out.println("Wynik dodawania to: " + calculator.add());
+        System.out.println("Wynik odejmowania to: " + calculator.substract());
     }
 }
