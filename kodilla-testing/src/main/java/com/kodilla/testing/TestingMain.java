e0dffbed71280579b1b81ebe2fe75da67896e3e7
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2020-06-11 14:28:44 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,8 +5,29 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args){
-        Calculator calculator = new Calculator(2,3);
-        System.out.println("Wynik dodawania to: " + calculator.add());
-        System.out.println("Wynik odejmowania to: " + calculator.substract());
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator(2, 3);
+        int addResult = calculator.add();
+        int substractResult = calculator.substract();
+
+        if(addResult == 5){
+            System.out.println("Test kalkulatora. Wartość oczekiwana: 5, wartość otrzymana: " + addResult + ". Test ok");
+        } else {
+            System.out.println("Błąd dodawania");
+        }
+
+        if(substractResult == -1){
+            System.out.println("Test kalkulatora. Wartość oczekiwana: -1, wartość otrzymana: " + substractResult + ". Test ok");
+        } else {
+            System.out.println("Błąd odejmowania");
+        }
     }
 }
