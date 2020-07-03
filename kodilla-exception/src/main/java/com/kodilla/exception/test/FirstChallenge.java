55fa97edb4b9cf18cd6865b68e1e1405d186ba8c
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2020-07-03 12:12:10 +0200

    8.2 Zadanie: try! catch! finally!- poprawka

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,24 +2,23 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
     public double divide(double a, double b) throws ArithmeticException {
-
-        try {
-            if (b == 0) {
-                throw new ArithmeticException();
-            }
-            return a / b;
-        } catch (ArithmeticException e) {
-            System.out.println("Błąd dzielenia przez 0");
+        if (b == 0) {
+            throw new ArithmeticException();
         }
-        return 0;
+        return a / b;
     }
+
         public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide(3, 0);
-
-        System.out.println(result);
-
+          try{
+              double result = firstChallenge.divide(3, 0);
+              System.out.println(result);
+          } catch (ArithmeticException e) {
+              System.out.println("Błąd dzielenia przez 0! " + e);
+          } finally {
+              System.out.println("Zadanie zakończone.");
+    }
     }
 }
