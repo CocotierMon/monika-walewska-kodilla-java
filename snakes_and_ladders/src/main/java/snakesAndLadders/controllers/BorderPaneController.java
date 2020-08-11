8d3be5b1531531e6bff7ccb80069cd13370eba45
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2020-08-11 11:18:56 +0200

    prawie skończone

--- snakes_and_ladders/src/main/java/snakesAndLadders/controllers/BorderPaneController.java
+++ snakes_and_ladders/src/main/java/snakesAndLadders/controllers/BorderPaneController.java
@@ -29,6 +29,7 @@ public class BorderPaneController {
     public Label result;
     public Label doMove;
     public Button go;
+    public Label winner;
 
     @FXML
     void initialize(){}
@@ -43,8 +44,9 @@ public class BorderPaneController {
             redCheckBox.setDisable(true);
             redPawn.setVisible(false);
             greenCheckBox.setDisable(true);
-            gridPane.add(purplePawn, 1, 9);
-            gridPane.add(greenPawn, 1, 9);
+            setPawn(purplePawn, 1, 9);
+            setPawn(greenPawn, 1, 9);
+            button.setVisible(true);
 
         } else if (blueCheckBox.isSelected() && nameBox.getText() != null){
             colour = "blue";
@@ -55,8 +57,9 @@ public class BorderPaneController {
             redCheckBox.setDisable(true);
             redPawn.setVisible(false);
             greenCheckBox.setDisable(true);
-            gridPane.add(bluePawn, 1, 9);
-            gridPane.add(greenPawn, 1, 9);
+            setPawn(bluePawn, 1, 9);
+            setPawn(greenPawn, 1, 9);
+            button.setVisible(true);
         } else if (redCheckBox.isSelected() && nameBox.getText() != null){
             colour = "red";
             playersList.setText(nameBox.getText() + " is " + colour);
@@ -66,8 +69,9 @@ public class BorderPaneController {
             purpleCheckBox.setDisable(true);
             purplePawn.setVisible(false);
             greenCheckBox.setDisable(true);
-            gridPane.add(redPawn, 1, 9);
-            gridPane.add(greenPawn, 1, 9);
+            setPawn(redPawn, 1, 9);
+            setPawn(greenPawn, 1, 9);
+            button.setVisible(true);
         } else if(greenCheckBox.isSelected() && nameBox.getText() != null){
             colour = "green";
             playersList.setText(nameBox.getText() + " is " + colour);
@@ -77,9 +81,17 @@ public class BorderPaneController {
             redCheckBox.setDisable(true);
             purplePawn.setVisible(false);
             purpleCheckBox.setDisable(true);
-            gridPane.add(greenPawn, 1, 9);
-            gridPane.add(redPawn, 1, 9);
+            setPawn(greenPawn, 1, 9);
+            setPawn(redPawn, 1, 9);
+            button.setVisible(true);
         }
+    }
+
+    private void setPawn(ImageView pawn, int column, int row) {
+        if (gridPane.getChildrenUnmodifiable().contains(pawn)) {
+            gridPane.getChildren().remove(pawn);
+        }
+        gridPane.add(pawn, column, row);
     }
 
     public static double createRandomNumber(double min, double max) {
@@ -88,35 +100,60 @@ public class BorderPaneController {
 
     public Label diceRolling(){
         if(button.isPressed()) {
-                result.setText(String.valueOf((int)createRandomNumber(1, 6)));
-                dice.setVisible(true);
-                doMove.setVisible(true);
+            result.setText(String.valueOf((int)createRandomNumber(1, 6)));
+            dice.setVisible(true);
+            doMove.setText("Move your pawn!");
+            doMove.setVisible(true);
+            go.setVisible(true);
+            button.setVisible(false);
+            changePositionPurple();
         }
         return result;
+    }
+
+    public Label computersDiceRolling(){
+
+            result.setText(String.valueOf((int)createRandomNumber(1, 6)));
+            doMove.setVisible(true);
+            go.setVisible(false);
+            button.setVisible(true);
+        changePositionGreen();
+
+        return result;
+    }
+    public void hide(){
+        go.setVisible(false);
+        button.setVisible(false);
+        dice.setVisible(false);
+        doMove.setText("Players 2 move");
+        computersDiceRolling();
+        computersMove();
     }
 
     public void moveThePane(){
 
         int move = Integer.parseInt(result.getText());
 
-        if(GridPane.getRowIndex(purplePawn)%2 == 0){ // jeśli wiersz jest parzysty (odejmujemy wynik rzutu)
-            if(GridPane.getColumnIndex(purplePawn) - move < 1){ // jeśli długość kolumny - wynik rzutu < 1
-                // - tutaj mam problem z obliczeniem docelowej kolumny, pozostałe warunki raczej działają raczej dobrze
+        if(GridPane.getRowIndex(purplePawn) == 0 && GridPane.getColumnIndex(purplePawn)-move<1){
 
-                int x = move - GridPane.getColumnIndex(purplePawn);
-                int column = 10-x-GridPane.getColumnIndex(purplePawn)+1;
+        } else
+
+        if(GridPane.getRowIndex(purplePawn)%2 == 0){ // jeśli wiersz jest parzysty
+                if(GridPane.getColumnIndex(purplePawn) - move < 1){ // jeśli długość kolumny - wynik rzutu < 1
+
+                int column = move - GridPane.getColumnIndex(purplePawn)+1;
                 int row = GridPane.getRowIndex(purplePawn)-1;
 
-                gridPane.add(purplePawn, column, row);
+                setPawn(purplePawn, column, row);
 
                 // jeśli długość kolumny - wynik rzutu > 1
             } else {
                 int column = GridPane.getColumnIndex(purplePawn) - move;
                 int row = GridPane.getRowIndex(purplePawn);
-                gridPane.add(purplePawn, column, row);
+                setPawn(purplePawn, column, row);
             }
         }
-        // jeśli wiersz jest nieparzysty (dodajemy wynik rzutu)
+        // jeśli wiersz jest nieparzysty
         else{
             if(GridPane.getColumnIndex(purplePawn) + move > 10){ // jeśli dlugość kolumny + wynik rzutu > 10
                 int x = 10 - GridPane.getColumnIndex(purplePawn);
@@ -125,14 +162,108 @@ public class BorderPaneController {
                 int column = 10 - y;
                 int row = GridPane.getRowIndex(purplePawn)-1;
 
-                gridPane.add(purplePawn, column, row);
+                setPawn(purplePawn, column, row);
 
                 // jeśli długość kolumny + wynik rzutu < 10
             } else {
                 int column = GridPane.getColumnIndex(purplePawn) + move;
                 int row = GridPane.getRowIndex(purplePawn);
-                gridPane.add(purplePawn, column, row);
+                setPawn(purplePawn, column, row);
             }
+        }
+    }
+
+     public void computersMove(){
+
+        int move = Integer.parseInt(result.getText());
+
+         if(GridPane.getRowIndex(greenPawn) == 0 && GridPane.getColumnIndex(greenPawn)-move<1){
+
+         } else
+        if(GridPane.getRowIndex(greenPawn)%2 == 0){ // jeśli wiersz jest parzysty
+             if(GridPane.getColumnIndex(greenPawn) - move < 1){ // jeśli długość kolumny - wynik rzutu < 1
+
+                int column = move - GridPane.getColumnIndex(greenPawn)+1;
+                int row = GridPane.getRowIndex(greenPawn)-1;
+
+                setPawn(greenPawn, column, row);
+
+                // jeśli długość kolumny - wynik rzutu > 1
+            } else {
+                int column = GridPane.getColumnIndex(greenPawn) - move;
+                int row = GridPane.getRowIndex(greenPawn);
+                setPawn(greenPawn, column, row);
+            }
+        }
+        // jeśli wiersz jest nieparzysty (dodajemy wynik rzutu)
+        else{
+            if(GridPane.getColumnIndex(greenPawn) + move > 10){ // jeśli dlugość kolumny + wynik rzutu > 10
+                int x = 10 - GridPane.getColumnIndex(greenPawn);
+                int y = move - x - 1;
+
+                int column = 10 - y;
+                int row = GridPane.getRowIndex(greenPawn)-1;
+
+                setPawn(greenPawn, column, row);
+
+                // jeśli długość kolumny + wynik rzutu < 10
+            } else {
+                int column = GridPane.getColumnIndex(greenPawn) + move;
+                int row = GridPane.getRowIndex(greenPawn);
+                setPawn(greenPawn, column, row);
+            }
+    }
+    }
+
+    // zachowanie pól specjalnych
+//21(1,7) -> 17(4,8), 30(10,7) -> 27(7,7), 94(7,0) -> 71(10,2), 98(3,0) -> 57(4,4)
+//16(5,8) -> 76(5,2), 33(8,6) -> 53(8,4), 39(2,6) -> 58(3,4), 55(6,4) -> 95(6,0)
+
+    public void changePositionPurple(){
+        if(GridPane.getColumnIndex(purplePawn) == 1 && GridPane.getRowIndex(purplePawn) == 7){
+            setPawn(purplePawn, 4, 8);
+        } else if (GridPane.getColumnIndex(purplePawn) == 10 && GridPane.getRowIndex(purplePawn) == 7){
+            setPawn(purplePawn, 7, 7);
+        } else if(GridPane.getColumnIndex(purplePawn) == 7 && GridPane.getRowIndex(purplePawn) == 0) {
+            setPawn(purplePawn, 10, 2);
+        } else  if(GridPane.getColumnIndex(purplePawn) == 3 && GridPane.getRowIndex(purplePawn) == 0){
+            setPawn(purplePawn, 4, 4);
+        } else if(GridPane.getColumnIndex(purplePawn) == 5 && GridPane.getRowIndex(purplePawn) == 8){
+            setPawn(purplePawn, 5, 2);
+        } else if(GridPane.getColumnIndex(purplePawn) == 8 && GridPane.getRowIndex(purplePawn) == 6){
+            setPawn(purplePawn, 8, 4);
+        } else if(GridPane.getColumnIndex(purplePawn) == 2 && GridPane.getRowIndex(purplePawn) == 6){
+            setPawn(purplePawn, 3, 4);
+        } else if(GridPane.getColumnIndex(purplePawn) == 6 && GridPane.getRowIndex(purplePawn) == 4){
+            setPawn(purplePawn, 6, 0);
+        } else if(GridPane.getColumnIndex(purplePawn) == 1 && GridPane.getRowIndex(purplePawn) == 0){
+            winner.setText("Congratulations! You won!");
+            winner.setVisible(true);
+            go.setVisible(false);
+        }
+    }
+
+    public void changePositionGreen(){
+        if(GridPane.getColumnIndex(greenPawn) == 1 && GridPane.getRowIndex(greenPawn) == 7){
+            setPawn(greenPawn, 4, 8);
+        } else if (GridPane.getColumnIndex(greenPawn) == 10 && GridPane.getRowIndex(greenPawn) == 7){
+            setPawn(greenPawn, 7, 7);
+        } else if(GridPane.getColumnIndex(greenPawn) == 7 && GridPane.getRowIndex(greenPawn) == 0){
+            setPawn(greenPawn, 10, 2);
+        } else if(GridPane.getColumnIndex(greenPawn) == 3 && GridPane.getRowIndex(greenPawn) == 0){
+            setPawn(greenPawn, 4, 4);
+        } else if(GridPane.getColumnIndex(greenPawn) == 5 && GridPane.getRowIndex(greenPawn) == 8){
+            setPawn(greenPawn, 5, 2);
+        } else if(GridPane.getColumnIndex(greenPawn) == 8 && GridPane.getRowIndex(greenPawn) == 6){
+            setPawn(greenPawn, 8, 4);
+        } else if(GridPane.getColumnIndex(greenPawn) == 2 && GridPane.getRowIndex(greenPawn) == 6){
+            setPawn(greenPawn, 3, 4);
+        } else if(GridPane.getColumnIndex(greenPawn) == 6 && GridPane.getRowIndex(greenPawn) == 4){
+            setPawn(greenPawn, 6, 0);
+        } else if(GridPane.getColumnIndex(greenPawn) == 1 && GridPane.getRowIndex(greenPawn) == 0){
+            winner.setText("The Computer won");
+            winner.setVisible(true);
+            go.setVisible(false);
         }
     }
 }
