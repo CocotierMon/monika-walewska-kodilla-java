c221cfbecca07619dffe0ee2efb39f70915b2be3
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2020-08-23 18:02:09 +0200

    14.2 Zadanie: Kalkulator z wyświetlaczem

---
+++ kodilla-stream/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@RunWith(org.springframework.test.context.junit4.SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Test
+    public void testCalculations(){
+        // Given
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = applicationContext.getBean(Calculator.class);
+        //When
+        double add = calculator.add(5,5);
+        double sub = calculator.sub(5,5);
+        double mul = calculator.mul(5,5);
+        double div = calculator.div(5,5);
+        //Then
+        Assert.assertEquals(10, add, 0.1);
+        Assert.assertEquals(0, sub, 0.1);
+        Assert.assertEquals(25, mul, 0.1);
+        Assert.assertEquals(1, div, 0.1);
+
+    }
+}
