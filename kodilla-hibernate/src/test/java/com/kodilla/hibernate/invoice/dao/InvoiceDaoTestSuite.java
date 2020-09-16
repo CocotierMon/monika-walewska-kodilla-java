a2721d65126538a9fb68e45e16e5de77439d1be0
CocotierMon <27955394+CocotierMon@users.noreply.github.com>
2020-09-16 13:17:37 +0200

    17.3 Zadanie: Nagłówki faktur oraz ich pozycje

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,50 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    private InvoiceDao invoiceDao;
+    @Autowired
+    private ProductDao productDao;
+    @Autowired
+    private ItemDao itemDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+        Product product1 = new Product("Produkt 1");
+        Item item1 = new Item(new BigDecimal(20), 2, new BigDecimal(40));
+        Item item2 = new Item(new BigDecimal(30), 2, new BigDecimal(60));
+        Invoice invoice1 = new Invoice("2020.09.16/1");
+        product1.getItems().add(item1);
+        item1.setProduct(product1);
+        invoice1.getItems().add(item1);
+        invoice1.getItems().add(item2);
+        item1.setInvoice(invoice1);
+        item2.setInvoice(invoice1);
+        //When
+        productDao.save(product1);
+        invoiceDao.save(invoice1);
+        itemDao.save(item1);
+        int id = invoice1.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
+        //CleanUp
+        invoiceDao.deleteById(id);
+    }
+}
